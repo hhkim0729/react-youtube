8ebf928b41e8972cacc6270491274605a438b102
hhkim0729 <hhkim0729@gmail.com>
2023-06-25 21:18:05 +0900

    Style header

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, createSearchParams } from 'react-router-dom';
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
 
 export default function SearchInput() {
   const [query, setQuery] = useState('');
@@ -21,9 +22,22 @@ export default function SearchInput() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={query} onChange={handleChange} />
-      <button type="submit">검색</button>
+    <form onSubmit={handleSubmit} className="flex basis-6/12">
+      <input
+        type="text"
+        value={query}
+        onChange={handleChange}
+        className="basis-10/12 border border-gray-300 border-r-0 rounded-l-3xl p-1 pl-5 text-lg text-gray-800"
+        placeholder="검색"
+      />
+      <button
+        type="submit"
+        className="w-16 bg-gray-100 border border-gray-300 rounded-r-3xl text-gray-500 hover:bg-gray-200"
+      >
+        <div className="flex justify-center">
+          <MagnifyingGlassIcon className="w-5" />
+        </div>
+      </button>
     </form>
   );
 }
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,16 +1,23 @@
 import { Outlet, Link } from 'react-router-dom';
+import { PlayCircleIcon } from '@heroicons/react/20/solid';
 import SearchInput from 'components/SearchInput';
 
 export default function Root() {
   return (
-    <>
-      <header>
-        <div>
-          <Link to="/">Youtube</Link>
+    <div className="container m-auto">
+      <header className="flex justify-center items-center gap-3 p-4">
+        <div className="flex items-center">
+          <PlayCircleIcon className="inline-block w-10 text-rose-400" />
+          <Link
+            to="/"
+            className="text-gray-500 text-2xl font-bold font-mono tracking-tighter"
+          >
+            LightTube
+          </Link>
         </div>
         <SearchInput />
       </header>
       <Outlet />
-    </>
+    </div>
   );
 }
