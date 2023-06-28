cf6888764e265610add29aadf232c28fba70e7ca
hhkim0729 <hhkim0729@gmail.com>
2023-06-28 23:06:53 +0900

    Style VideoList

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,21 +1,28 @@
 import { Link } from 'react-router-dom';
+import { truncate } from 'utils';
 
 export default function VideoList({ videos }) {
   return (
-    <ul>
+    <ul className="flex flex-wrap gap-3 justify-center">
       {videos.map((video) => {
         const { id, snippet, statistics } = video;
         const videoId = id.videoId ? id.videoId : id;
         return (
-          <li key={videoId}>
+          <li key={videoId} className={`max-w-[320px] min-w-[200px] pb-2`}>
             <Link to={`/watch/${videoId}`} state={{ video }}>
-              <img src={snippet.thumbnails.medium.url} alt={snippet.title} />
+              <img
+                src={snippet.thumbnails.medium.url}
+                alt={snippet.title}
+                className="rounded-lg"
+              />
             </Link>
-            <div>
+            <div className="p-2">
               <Link to={`/watch/${videoId}`} state={{ video }}>
-                <h3>{snippet.title}</h3>
+                <h3 className="break-all text-lg font-medium leading-6">
+                  {truncate(snippet.title, 63)}
+                </h3>
               </Link>
-              <div>
+              <div className="text-sm text-gray-600 mt-1">
                 <span>{snippet.channelTitle}</span>
                 <div>
                   {statistics && (
