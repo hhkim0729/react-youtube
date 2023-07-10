d11d5a204bf063442bc50a9b9829f94aa910efad
hhkim0729 <hhkim0729@gmail.com>
2023-07-10 21:52:07 +0900

    Fix style

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,7 +4,7 @@ export default function Error() {
   let error = useRouteError();
   console.error(error);
   return (
-    <div className="container flex flex-col items-center h-screen mt-52">
+    <div className="w-full flex flex-col items-center h-screen mt-52">
       <h1 className="text-4xl font-medium mb-8">Error!</h1>
       <h2 className="text-3xl text-gray-500 font-medium mb-4">
         {error.status} {error.statusText}
